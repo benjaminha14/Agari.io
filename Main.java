2ef092568143d4635a94d7dca4e5fa678ed9f34a
benjaminha14 <benjaminha14@gmail.com>
2015-11-17 10:57:51 -0800

    Main Game Class
    Put all init here

---
+++ Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+	
+	public static void main(String args[])
+   {
+	   init();
+	}
+   public static void init()
+   {
+     JFrame frame = new JFrame("Agar.io");
+     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     frame.pack();
+     frame.setVisible(true);
+     frame.getContentPane().setBackground(new Color(240,234,214));
+     frame.setSize(800,600);	
+   }
+   
+
+}
